3146872353fb982add6d99e66a29e75e58287ad8
Omer Cagri Sayir <omercagris@gmail.com>
2023-10-20 16:44:04 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import '@mantine/core/styles.css';
-import { Button } from '@mantine/core';
-
-import { MantineProvider } from '@mantine/core';
-
 function App() {
-	return (
-		<MantineProvider>
-			<h1 className='text-3xl font-bold underline'>Hello</h1>
-			<Button variant='filled' className='bg-red-300'>
-				Button
-			</Button>
-			;
-		</MantineProvider>
-	);
+	return <div className='h-full bg-cyan-300'></div>;
 }
 
 export default App;
